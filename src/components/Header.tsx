ecefe852b062cf8e1a4362579c93d56828ab0bd5
Thiago Coelho <thiago.coelho158@gmail.com>
2025-01-31 17:39:20 -0300

    feat: adicionar novos links ao Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,13 @@
+'use client'
+
+import api from "@/lib/api";
 import logo from "../app/images/logo.png"
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 export function Header() {
+    const router = useRouter();
+
     const pages = [
         {
             "name": "Consultas",
@@ -10,7 +17,30 @@ export function Header() {
             "name": "Pacientes",
             "href": "/app/pacientes",
         },
+        {
+            "name": "Médicos",
+            "href": "/app/medicos",
+        },
+        {
+            "name": "Usuários",
+            "href": "/app/usuarios",
+        },
     ];
+
+    function logout() {
+        api.get('/auth/logout').then(
+            (response) => {
+                if (response.status === 200) {
+                    router.push("/")
+                }
+            }
+        ).catch(
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
+
     return (
         <div className="fixed bg-white w-full h-16 shadow flex items-center z-30">
             <img src={logo.src} className="h-14 ml-1 bg-blue-500 rounded-lg"/>
@@ -26,7 +56,7 @@ export function Header() {
                     );
                 })}
             </ul>
-            <a href="/" className="text-black ml-auto mr-8 hover:text-blue-700 font-medium p-4">Sair</a>
+            <Button className="ml-auto mr-8" variant={"ghost"} onClick={logout}>Sair</Button>
         </div>
     );
 }
